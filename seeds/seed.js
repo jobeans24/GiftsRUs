41e11d97de398f2d7f3bd0f094361809b20a7fa8
fhubert1 <f.hubert.712@gmail.com>
2024-05-26 21:15:05 -0400

    complete models and seeds file

---
+++ seeds/seed.js
@@ -0,0 +1,26 @@
+const sequelize = require('../config/connection');
+const { Event, Gift, Location, Purchased, Cart, User } = require('../models');
+
+// bring in the json data for each table
+const cartData = require('./cartData.json');
+const eventData = require('./eventData.json');
+const giftData = require('./giftData.json');
+const locationData = require('./locationData.json');
+const purchasedData = require('./purchasedData.json');
+const userData = require('./userData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+  
+    // the order of the first couplde of bulk creates is critical can't create a location or event without out having a user
+    await User.bulkCreate(userData, { individualHooks: true });
+    await Location.bulkCreate(locationData);
+    await Event.bulkCreate(eventData);
+    await Gift.bulkCreate(giftData);
+    await Cart.bulkCreate(cartData, { individualHooks: true });
+    await Purchased.bulkCreate(purchasedData);
+  
+    process.exit(0);
+  };
+  
+  seedDatabase();
