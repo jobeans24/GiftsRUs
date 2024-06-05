6cbea5e7b4a6c13c0a2711ba76d08f1a4f468fe7
Josie Zobel <159351625+jobeans24@users.noreply.github.com>
2024-06-05 12:12:31 -0400

    adding destroy session to userRoutes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -88,6 +88,13 @@ router.delete('/:id', async (req, res) => {
                 res.status(204).end();
             });
         }
+       
+        // destory session
+        if (req.session.logged_in) {
+            req.session.destroy(() =>{
+                res.status(204).end();
+            });
+        } 
     } catch (err) {
         res.status(500).json(err);
     }
